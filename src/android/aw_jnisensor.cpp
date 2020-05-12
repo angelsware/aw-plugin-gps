c0f119db2c70d68d2e42ac8722a6c15109de2c58
Joachim Klahr <joachim.klahr@gmail.com>
2020-05-12 15:09:36 +0200

    Added missing parameter.

--- src/android/aw_jnisensor.cpp
+++ src/android/aw_jnisensor.cpp
@@ -1,7 +1,7 @@
 #include "aw_jnisensor.h"
 #include <gps/aw_sensorlistener.h>
 
-JNIEXPORT void JNICALL Java_com_angelsware_gps_Sensor_onGpsLocation(JNIEnv* env, jclass clazz, jdouble latitude, jdouble longitude, jlong timeSinceLastUpdate, jlong listener) {
+JNIEXPORT void JNICALL Java_com_angelsware_gps_Sensor_onGpsLocation(JNIEnv* env, jclass clazz, jdouble latitude, jdouble longitude, jlong timeSinceLastUpdate, jfloat accuracy, jlong listener) {
 	if (Gps::ISensorListener* ptr = reinterpret_cast<Gps::ISensorListener*>(listener)) {
 		ptr->onGpsLocation(latitude, longitude, timeSinceLastUpdate, accuracy);
 	}
